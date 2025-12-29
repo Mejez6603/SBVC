1da65f3aaebd6ed66745d9f77cd5fb11b1ca2450
MEJES, JERAHMEEL ALZEL, M <jerahmeel6603@gmail.com>
2025-12-29 14:30:29 +0000

    the box are still there remove this <div data-state="inactive" data-ori

--- src/app/controller/page.tsx
+++ src/app/controller/page.tsx
@@ -116,7 +116,7 @@ function Controller() {
                     <CustomizationController />
                 </ScrollArea>
             </TabsContent>
-            <TabsContent value="ai" className="flex-1 flex flex-col">
+            <TabsContent value="ai" className="flex-1 flex flex-col min-h-0">
                 <Notepad />
             </TabsContent>
           </Tabs>
